b00dfb3ba645df6d7856abdb0b6a89bda62b36d3
Augalassia <doaakhalaf1outlook@gmail.com>
2024-05-20 11:39:08 +0300

    add orders

---
+++ assets/order.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+        productName: 'Full-Stack',
+        productNumber: '86251',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    {
+        productName: 'Full-Stack',
+        productNumber: '86251',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
